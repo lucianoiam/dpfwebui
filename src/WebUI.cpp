1f041c8df3033d93cb07a5e0c094ca6a25af45d0
Luciano Iam <lucianito@gmail.com>
2021-05-14 17:54:35 +0200

    Include Linux helper recipe in Makefile

--- src/WebUI.cpp
+++ src/WebUI.cpp
@@ -73,7 +73,7 @@ WebUI::WebUI()
     sprintf(strWindowId, "%lx", (long)windowId);
     pid_t pid;
     const char *argv[] = {"helper", strWindowId, CONTENT_URL, NULL};
-    const char* fixmeHardcodedPath = "/home/user/src/dpf-webui/bin/helper";
+    const char* fixmeHardcodedPath = "/home/user/src/dpf-webui/bin/d_dpf_webui_helper";
     int status = posix_spawn(&pid, fixmeHardcodedPath, NULL, NULL, (char* const*)argv, environ);
     syslog(LOG_INFO, "posix_spawn() status %d\n", status);
 #endif
