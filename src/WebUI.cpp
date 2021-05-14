cdd2857b8419661a26db33595bb1d9a1b68ed1a7
Luciano Iam <lucianito@gmail.com>
2021-05-14 14:59:36 +0200

    Fix compilation on Linux
    Warning: the Linux helper still needs to be manually compiled!
    Need to add it to the Makefile

--- src/WebUI.cpp
+++ src/WebUI.cpp
@@ -30,6 +30,7 @@
 
 #ifdef DISTRHO_OS_LINUX
 // TODO 2: move linux specific code to separate file
+#include <syslog.h>
 #include <spawn.h>
 extern char **environ;
 #endif
@@ -44,8 +45,6 @@ extern char **environ;
 #include "macos/WebView.h"
 bool _displayed = false;
 #endif
-
-//#include <syslog.h>
 
 #define CONTENT_URL "https://distrho.sourceforge.io/images/screenshots/distrho-kars.png"
 
