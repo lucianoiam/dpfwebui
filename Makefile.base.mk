f20b78b122dacfb1329b50aa336c5031c0d100a5
Luciano Iam <lucianito@gmail.com>
2021-05-14 15:23:28 +0200

    Fix Makefile indentation

--- Makefile.base.mk
+++ Makefile.base.mk
@@ -1,3 +1,13 @@
+# Sanity checks
+ifeq (,$(wildcard $(DPF_CUSTOM_PATH)/Makefile))
+$(error DPF not found, make sure it was cloned as a submodule)
+endif
+
+# This is needed until the DPF_CUSTOM_PATH support patch is merged into DPF
+ifneq (,$(findstring master,$(shell git -C $(DPF_CUSTOM_PATH) branch --show-current)))
+$(error DPF branch is master, make sure its current branch is develop)
+endif
+
 TARGET_MACHINE := $(shell gcc -dumpmachine)
 
 ifneq (,$(findstring linux,$(TARGET_MACHINE)))
@@ -16,13 +26,4 @@ ifeq ($(WINDOWS),true)
 ifeq (,$(wildcard ./lib/windows/WebView2))
 $(error WebView2 SDK not found, please have a look at BUILD.txt)
 endif
-endif
-
-ifeq (,$(wildcard $(DPF_CUSTOM_PATH)/Makefile))
-$(error DPF not found, make sure it was cloned as a submodule)
-endif
-
-# This is needed until the DPF_CUSTOM_PATH support patch is merged into DPF
-ifneq (,$(findstring master,$(shell git -C $(DPF_CUSTOM_PATH) branch --show-current)))
-$(error DPF branch is master, make sure its current branch is develop)
 endif
