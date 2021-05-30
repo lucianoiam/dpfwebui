c0ce60dd3d84c4f1dfa09920db3ff6dce22f7f8b
Luciano Iam <lucianoiam@protonmail.com>
2021-05-31 01:01:28 +0200

    Use visibility trick to reduce flicker on Windows
    Now all three platforms do the same

--- src/windows/event.h
+++ src/windows/event.h
@@ -1,4 +1,5 @@
-// https://github.com/jchv/webview2-in-mingw/blob/master/Src/event.h
+// Based on https://github.com/jchv/webview2-in-mingw
+// lucianoiam - more event types and __EVENT__ template
 #pragma once
 #define CINTERFACE
 #include <functional>
@@ -6,7 +7,10 @@
 #include "WebView2.h"
 
 class EventHandler : public ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler,
-                     public ICoreWebView2CreateCoreWebView2ControllerCompletedHandler
+                     public ICoreWebView2CreateCoreWebView2ControllerCompletedHandler,
+                     public ICoreWebView2ContentLoadingEventHandler,
+                     public ICoreWebView2NavigationCompletedEventHandler
+                  // public ICoreWebView2__EVENT__Handler
 {
 public:
     EventHandler();
@@ -19,4 +23,7 @@ public:
 
     std::function<HRESULT(HRESULT result, ICoreWebView2Environment* created_environment)> EnvironmentCompleted;
     std::function<HRESULT(HRESULT result, ICoreWebView2Controller* controller)> ControllerCompleted;
+    std::function<HRESULT(ICoreWebView2 *sender, ICoreWebView2ContentLoadingEventArgs *args)> ContentLoading;
+    std::function<HRESULT(ICoreWebView2 *sender, ICoreWebView2NavigationCompletedEventArgs *args)> NavigationCompleted;
+ // std::function<HRESULT(HRESULT result, ... see WebView2.h for arguments ... )> __EVENT__;
 };
