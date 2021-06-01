5d458d4c59f0559098407b94323eefe65992ed69
Luciano Iam <lucianoiam@protonmail.com>
2021-06-01 21:33:57 +0200

    Improve readability of WebViewHandler

--- src/windows/WebViewHandler.hpp
+++ src/windows/WebViewHandler.hpp
@@ -37,7 +37,7 @@ namespace edge {
 class WebViewHandler : public ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler,
                        public ICoreWebView2CreateCoreWebView2ControllerCompletedHandler,
                        public ICoreWebView2NavigationCompletedEventHandler
-                       //public ICoreWebView2__EVENT__Handler
+                       //public ICoreWebView2[ EVENT ]Handler
 {
 public:
     WebViewHandler();
@@ -67,7 +67,7 @@ public:
         return S_OK;
     }
 
-    /*virtual HRESULT handleWebView__EVENT__( ... see WebView2.h for arguments ... )
+    /*virtual HRESULT handleWebView[ EVENT ]([ ARGS ])
     {
         return S_OK;
     }*/
