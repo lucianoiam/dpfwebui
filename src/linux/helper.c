0a02e12807786939608c0bd80de3c6da7af21f6c
Luciano Iam <lucianito@gmail.com>
2021-06-06 01:13:17 +0200

    Remove fflush() debug calls

--- src/linux/helper.c
+++ src/linux/helper.c
@@ -183,7 +183,6 @@ static void web_view_script_message_cb(WebKitUserContentManager *manager, WebKit
             }
         }
     }
-    fflush(stdout);
     ipc_write_simple(handler_ctx->ctx, OPC_HANDLE_SCRIPT_MESSAGE, payload, payload_sz);
     free(payload);
     webkit_javascript_result_unref(res);
@@ -275,5 +274,5 @@ static void serialize_jsc_value(JSCValue *value, char **buf, int *offset)
         //printf("Tx (str) %s\n", *buf+*offset);
         *offset += sz;
     }
-    fflush(stdout);
+    //fflush(stdout);
 }
