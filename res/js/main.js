fa1e8628d3c8e8232df40e7950ac6069da8add60
Luciano Iam <lucianito@gmail.com>
2021-06-06 01:36:26 +0200

    Update console greeting

--- res/js/main.js
+++ res/js/main.js
@@ -1,5 +1,5 @@
 {
     const main = document.getElementById('main');
     main.innerHTML = `<h1>${window._testString}</h1>`;
-    console.log('console test');
+    console.log(window._testString);
 }
