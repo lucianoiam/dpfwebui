664d246bad2c1385a4d36c72d349bad5df14f1c8
Luciano Iam <lucianito@gmail.com>
2021-06-06 11:47:58 +0200

    Use uppercase DPF as message handler name

--- res/js/main.js
+++ res/js/main.js
@@ -2,5 +2,5 @@
     const main = document.getElementById('main');
     main.innerHTML = `<h1>${window._testString}</h1>`;
     console.log(window._testString); // window.webkit.messageHandlers.console_log
-    window.webkit.messageHandlers.dpf.postMessage(['setParameterValue', 2, 0.5]);
+    window.webkit.messageHandlers.DPF.postMessage(['setParameterValue', 2, 0.5]);
 }
