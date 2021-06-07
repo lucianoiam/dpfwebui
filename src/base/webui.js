15f9c6a3cbe19f69fee785bf9504f1968ad7c108
Luciano Iam <lucianito@gmail.com>
2021-06-08 01:26:44 +0200

    Minor refactor
    Seems Windows is sporadically hanging again

--- src/base/webui.js
+++ src/base/webui.js
@@ -39,14 +39,14 @@ class WebUI {
         // default empty implementation
     }
 
-    // (cpp->js) BaseWebView::postMessage(const ScriptValueVector& args)
-    messageReceived(args) {
-        // default empty implementation
-    }
-
-    // (js->cpp) WebViewEventHandler::webViewScriptMessageReceived(const ScriptValueVector& args)
+    // BaseWebView::postMessage(const ScriptValueVector& args)
     postMessage(...args) {
         window.webviewHost.postMessage(args);
+    }
+
+    // WebViewEventHandler::webViewScriptMessageReceived(const ScriptValueVector& args)
+    messageReceived(args) {
+        // default empty implementation
     }
 
 }
