92338735172be81644a7af3ef7a2de2769528594
Luciano Iam <lucianito@gmail.com>
2021-06-07 19:20:53 +0200

    Update some copyright headers

--- src/base/WebUI.hpp
+++ src/base/WebUI.hpp
@@ -15,9 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
- * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
 
 #ifndef WEBUI_HPP
 #define WEBUI_HPP
