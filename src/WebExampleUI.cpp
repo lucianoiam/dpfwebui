22048d761aa6aefc61eafce12aa10b53061b6bbd
Luciano Iam <lucianito@gmail.com>
2021-06-09 11:24:55 +0200

    Update comment on API usage

--- src/WebExampleUI.cpp
+++ src/WebExampleUI.cpp
@@ -35,18 +35,18 @@ UI* DISTRHO::createUI()
 WebExampleUI::WebExampleUI()
     : WebUI(INIT_WIDTH_PX, INIT_HEIGHT_PX, INIT_BACKGROUND_RGBA)
 {
-    // Web view is not guaranteed to be ready yet. Calls to webView().runScript()
-    // or any mapped WebUI methods are forbidden. Mapped methods are those that
-    // have their JavaScript counterparts; they rely on message passing and
-    // ultimately webView().runScript(). Still can call webView().injectScript()
-    // to queue scripts that will run immediately after content finishes loading
-    // and before any referenced scripts ( <script src="..."> ) start running.
+    // Web view not guaranteed to be ready yet. Calls to webView().runScript()
+    // or any DPF methods mapped by WebUI are forbidden. Mapped methods are
+    // those that have their JavaScript counterparts; they rely on message
+    // passing and ultimately webView().runScript(). Still it is safe to call
+    // webView().injectScript() to queue scripts that will run immediately after
+    // web content finishes loading and before any other <script> starts running.
 }
 
 void WebExampleUI::webContentReady()
 {
     // Called when the main document finished loading and DOM is ready. It is
-    // now safe to call webView().runScript() if needed.
+    // now safe to call webView().runScript() and mapped DPF methods if needed.
 }
 
 void WebExampleUI::webMessageReceived(const ScriptValueVector& args)
