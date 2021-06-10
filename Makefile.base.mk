f04386c588580e70f2a4bed3a6e49d91e5375d17
Luciano Iam <lucianito@gmail.com>
2021-06-10 11:08:09 +0200

    Makefiles remix

--- Makefile.base.mk
+++ Makefile.base.mk
@@ -1,3 +1,4 @@
+# Created by lucianoiam
 TARGET_MACHINE := $(shell gcc -dumpmachine)
 
 ifneq (,$(findstring linux,$(TARGET_MACHINE)))
@@ -18,10 +19,18 @@ endif
 
 ifeq ($(MSYS_MINGW),true)
 ifeq ($(shell cmd /c "net.exe session 1>NUL 2>NUL || exit /b 1"; echo $$?),1)
-$(info If you want real symlink support re-run MSYS as administrator)
+$(info For real symlink support re-run MSYS as administrator)
 MSYS_MINGW_SYMLINKS=:
 else
 MSYS_MINGW_SYMLINKS=export MSYS=winsymlinks:nativestrict
+endif
+endif
+
+ifneq ($(MSYS_MINGW),true)
+ifneq ($(CROSS_COMPILING),true)
+CAN_GENERATE_TTL = true
+else ifneq ($(EXE_WRAPPER),)
+CAN_GENERATE_TTL = true
 endif
 endif
 
@@ -32,30 +41,5 @@ endif
 ifeq ($(USE_DPF_DEVELOP_BRANCH),true)
 ifeq (,$(findstring develop,$(shell git -C $(DPF_CUSTOM_PATH) branch --show-current)))
 _:=$(shell git -C $(DPF_CUSTOM_PATH) checkout develop)
-endif
-endif
-
-ifeq ($(WINDOWS),true)
-  EDGE_WEBVIEW2_PATH=./lib/Microsoft.Web.WebView2
-  ifeq (,$(wildcard $(EDGE_WEBVIEW2_PATH)))
-    ifeq (,$(shell which nuget 2>/dev/null))
-      ifeq ($(MSYS_MINGW),true)
-        $(info Downloading NuGet...)
-        _:=$(shell wget -P /usr/bin https://dist.nuget.org/win-x86-commandline/latest/nuget.exe)
-      else
-         $(error NuGet not found, try sudo apt install nuget or the equivalent for your distro.)
-      endif
-    endif
-    $(info Downloading Edge WebView2 SDK...)
-    _:=$(shell nuget install Microsoft.Web.WebView2 -OutputDirectory lib)
-    _:=$(shell $(MSYS_MINGW_SYMLINKS); ln -rs $(EDGE_WEBVIEW2_PATH).* $(EDGE_WEBVIEW2_PATH))
-  endif
-endif
-
-ifneq ($(MSYS_MINGW),true)
-ifneq ($(CROSS_COMPILING),true)
-CAN_GENERATE_TTL = true
-else ifneq ($(EXE_WRAPPER),)
-CAN_GENERATE_TTL = true
 endif
 endif
