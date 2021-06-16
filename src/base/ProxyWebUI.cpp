ac7bf4ab0ec2debd25fd4533234b5f078b4c560e
Luciano Iam <oss@lucianoiam.com>
2021-06-17 00:08:51 +0200

    Revert 9bf9f5e

--- src/base/ProxyWebUI.cpp
+++ src/base/ProxyWebUI.cpp
@@ -41,8 +41,8 @@ ProxyWebUI::ProxyWebUI(uint baseWidth, uint baseHeight, uint32_t backgroundColor
     uint width = k * baseWidth;
     uint height = k * baseHeight;
     setGeometryConstraints(width, height, true);
+    setSize(width, height);
     fWebWidget.setEventHandler(this);
-    //fWebWidget.resize(getSize());
     fWebWidget.setBackgroundColor(fBackgroundColor);
     String js = String(
 #include "base/webui.js"
