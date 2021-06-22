33315f9cb734e78b40488800f34d3c634f796d23
Luciano Iam <oss@lucianoiam.com>
2021-06-23 00:57:53 +0200

    Add scaffold code for a resize handle

--- src/ui/web-gain-example-ui.js
+++ src/ui/web-gain-example-ui.js
@@ -43,7 +43,7 @@ class WebGainExampleUI extends DISTRHO_WebUI {
         document.body.style.visibility = 'visible';
 
         this.isResizable().then((result) => {
-        	console.log('isResizable() = ' + result);
+            new ResizeHandle(this);
         });
     }
 
