cc70643c1dca06297c3901b6c91b6c69306371e2
Luciano Iam <oss@lucianoiam.com>
2021-06-23 00:35:34 +0200

    Expose UI::isResizable() to JS
    Include a generic mechanism for dealing with sync calls to UI

--- src/base/ProxyWebUI.cpp
+++ src/base/ProxyWebUI.cpp
@@ -46,8 +46,8 @@ namespace DISTRHO {
 ProxyWebUI::ProxyWebUI(uint baseWidth, uint baseHeight, uint32_t backgroundColor)
     : UI(INIT_SCALE_FACTOR * baseWidth, INIT_SCALE_FACTOR * baseHeight)
     , fWebWidget(this)
+    , fFlushedInitMsgQueue(false)
     , fBackgroundColor(backgroundColor)
-    , fInitContentReady(false)
 {
     setGeometryConstraints(getWidth(), getHeight(), false, false);
 
@@ -99,7 +99,7 @@ void ProxyWebUI::stateChanged(const char* key, const char* value)
 #endif // DISTRHO_PLUGIN_WANT_STATE == 1
 
 void ProxyWebUI::webPostMessage(const ScriptValueVector& args) {
-    if (fInitContentReady) {
+    if (fFlushedInitMsgQueue) {
         fWebWidget.postMessage(args);
     } else {
         fInitMsgQueue.push_back(args);
@@ -108,6 +108,12 @@ void ProxyWebUI::webPostMessage(const ScriptValueVector& args) {
 
 void ProxyWebUI::flushInitMessageQueue()
 {
+    if (fFlushedInitMsgQueue) {
+        return;
+    }
+
+    fFlushedInitMsgQueue = true;
+
     for (InitMessageQueue::iterator it = fInitMsgQueue.begin(); it != fInitMsgQueue.end(); ++it) {
         fWebWidget.postMessage(*it);
     }
@@ -117,7 +123,7 @@ void ProxyWebUI::flushInitMessageQueue()
 
 void ProxyWebUI::handleWebWidgetContentLoadFinished()
 {
-    fInitContentReady = true;
+    // no-op, just let derived classes now
     webContentReady();
 }
 
@@ -133,6 +139,11 @@ void ProxyWebUI::handleWebWidgetScriptMessageReceived(const ScriptValueVector& a
 
     if (method == "flushInitMessageQueue") {
         flushInitMessageQueue();
+
+    } else if (method == "isResizable") {
+        // It is not possible to implement synchronous calls without
+        // resorting to dirty hacks, fulfill getter promises instead.
+        webPostMessage({"WebUI", "isResizable", isResizable()});
 
     } else if ((method == "setSize") && (argc == 2)) {
         setSize(
