5f964fbdfadd2354f69c4ccb9b4725f979cce9c5
Luciano Iam <oss@lucianoiam.com>
2021-06-23 15:26:42 +0200

    Fix resize handle on Linux
    Actually improve implementation by not assuming clientWidth/clientHeight can be used to establish minimum width/height because on Linux due to the way the webview works, client* can return incorrect values. Query DistrhoUI instead.
    
    Thinking twice, the Linux helper should accept initial window width/height as process parameters to avoid resizing the window (and the associated visual glitch). After updating that, clientWidth/clientHeight will be valid.

--- src/base/webui.js
+++ src/base/webui.js
@@ -40,6 +40,16 @@ class DISTRHO_WebUI {
         });
     }
 
+    // UI::getWidth()
+    async getWidth() {
+        return this._callWithReply('getWidth');
+    }
+
+    // UI::getHeight()
+    async getHeight() {
+        return this._callWithReply('getHeight');
+    }
+
     // UI::isResizable()
     async isResizable() {
         return this._callWithReply('isResizable');
