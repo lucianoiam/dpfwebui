3a83961f47199e583396db7052354e4e1bd3d63f
Luciano Iam <oss@lucianoiam.com>
2021-06-29 02:42:12 +0200

    Start trying to fix Linux kbd input
    Not working on Bitwig. Working on Carla and standalone. REAPER seems broken for any plugin.

--- src/arch/linux/ExternalGtkWebWidget.hpp
+++ src/arch/linux/ExternalGtkWebWidget.hpp
@@ -38,6 +38,7 @@ public:
     ~ExternalGtkWebWidget();
 
     void onResize(const ResizeEvent& ev) override;
+    bool onKeyboard(const KeyboardEvent& ev) override;
 
     void setBackgroundColor(uint32_t rgba) override;
     void navigate(String& url) override;
