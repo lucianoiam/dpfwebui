a54e9f161aca5cd6b845b14a6e27210beff86c1b
Luciano Iam <oss@lucianoiam.com>
2021-07-11 14:28:30 +0200

    Workaround for empty view on Windows JACK app

--- src/windows/EdgeWebWidget.hpp
+++ src/windows/EdgeWebWidget.hpp
@@ -75,6 +75,7 @@ protected:
     bool onKeyboard(const KeyboardEvent& ev) override;
 
 private:
+    void initWebView();
     void updateWebViewBounds();
     
     void webViewLoaderErrorMessageBox(HRESULT result);
