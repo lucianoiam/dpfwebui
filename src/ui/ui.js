8d10fe98d6f117d7efe3d786ab0f79eeb60594a2
Luciano Iam <oss@lucianoiam.com>
2021-07-19 22:21:22 +0200

    Create separate file for ui.js include
    Leave ui.js legal JavaScript and dynamically create the heredoc

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
- */
 
 class DISTRHO_UI {
 
@@ -147,5 +146,3 @@ class DISTRHO_UI {
     }
 
 }
-
-)UI_JS"
