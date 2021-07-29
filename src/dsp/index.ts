ca5824bb312b6a62a14d6ba95dac856d41eeb55e
Luciano Iam <oss@lucianoiam.com>
2021-07-29 13:38:10 +0200

    Remove unnecessary code

--- src/dsp/index.ts
+++ src/dsp/index.ts
@@ -131,14 +131,6 @@ const MAX_PROCESS_BLOCK_SIZE = 65536
 export let _input_block = new ArrayBuffer(MAX_PROCESS_BLOCK_SIZE)
 export let _output_block = new ArrayBuffer(MAX_PROCESS_BLOCK_SIZE)
 
-// TypedArray exports needed by the JS loader
-// FIXME: these do not belong here, a custom index.ts should be created for
-// projects that need additional low-level interfacing with the Wasm module.
-// A possible solution is importing framework's index.ts and re-exporting all
-// its imports then adding custom definitions.
-export let _input_block_float32 = Float32Array.wrap(_input_block)
-export let _output_block_float32 = Float32Array.wrap(_output_block)
-
 // AssemblyScript does not support multi-values yet. Export a couple of generic
 // variables for returning complex data types like initParameter() needs.
 
