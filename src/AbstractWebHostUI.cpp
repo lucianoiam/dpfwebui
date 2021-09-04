5a3d3cfea1b528fbf74f6ed24c37e693b055468d
Luciano Iam <oss@lucianoiam.com>
2021-09-04 23:59:57 +0200

    Bugfix: background color on Windows

--- src/AbstractWebHostUI.cpp
+++ src/AbstractWebHostUI.cpp
@@ -33,6 +33,13 @@ void AbstractWebHostUI::initWebView(AbstractWebView& webView)
 {
     uintptr_t parent = isStandalone() ? createStandaloneWindow() : getParentWindowHandle();
 
+    webView.setBackgroundColor(fBackgroundColor);
+    webView.setParent(parent);
+    webView.setEventHandler(this);
+#ifdef HIPHOP_PRINT_TRAFFIC
+    webView.setPrintTraffic(true);
+#endif
+
     // Web views adjust their contents following the system display scale factor,
     // adjust window size so it correctly wraps content on high density displays.
     float k = getDisplayScaleFactor(parent);
@@ -40,12 +47,8 @@ void AbstractWebHostUI::initWebView(AbstractWebView& webView)
     fInitHeight = k * getHeight();
     setSize(fInitWidth, fInitHeight);
 
+#ifdef DISTRHO_OS_WINDOWS
     webView.setSize(fInitWidth, fInitHeight);
-    webView.setBackgroundColor(fBackgroundColor);
-    webView.setParent(parent);
-    webView.setEventHandler(this);
-#ifdef HIPHOP_PRINT_TRAFFIC
-    webView.setPrintTraffic(true);
 #endif
 
     String js = String(
