5338f1540100010bb176b055c37f17336d8b9463
Luciano Iam <oss@lucianoiam.com>
2021-09-17 15:09:02 +0200

    Rename MacPath.mm to MacPath.cpp
    Also remove #import <AppKit/AppKit.h>. MacPath does not call any Cocoa
    APIs nor contains Objective-C code.

--- Makefile.plugins.mk
+++ Makefile.plugins.mk
@@ -80,7 +80,7 @@ ifeq ($(LINUX),true)
 HIPHOP_FILES_DSP += linux/LinuxPath.cpp
 endif
 ifeq ($(MACOS),true)
-HIPHOP_FILES_DSP += macos/MacPath.mm
+HIPHOP_FILES_DSP += macos/MacPath.cpp
 endif
 ifeq ($(WINDOWS),true)
 HIPHOP_FILES_DSP += windows/WindowsPath.cpp
@@ -103,7 +103,7 @@ HIPHOP_FILES_UI += linux/LinuxPath.cpp \
                    linux/ipc.c
 endif
 ifeq ($(MACOS),true)
-HIPHOP_FILES_UI += macos/MacPath.mm \
+HIPHOP_FILES_UI += macos/MacPath.cpp \
                    macos/MacWebHostUI.mm \
                    macos/CocoaWebView.mm
 endif
